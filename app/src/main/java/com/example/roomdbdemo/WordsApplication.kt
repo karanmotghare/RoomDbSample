7df83b431e9bcda007c4aef48a61ae87c1326b0c
karan <karanmotghare8@gmail.com>
2022-05-26 10:10:10 +0530

    final

---
+++ app/src/main/java/com/example/roomdbdemo/WordsApplication.kt
@@ -0,0 +1,13 @@
+package com.example.roomdbdemo
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class WordsApplication : Application() {
+
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
